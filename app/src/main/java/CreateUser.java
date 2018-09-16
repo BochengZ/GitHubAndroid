8fa24fbe35e7b06da11ed53eb568d850d615b300
BOCHENG ZHU <jerry.zbc111@gmail.com>
2018-09-16 18:46:05 +1000

    create user

---
+++ app/src/main/java/CreateUser.java
@@ -0,0 +1,17 @@
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.sairamkrishna.famouspeople.R;
+
+/**
+ * Created by BOCHENG ZHU on 16/09/2018.
+ */
+
+public class CreateUser extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_user);
+    }
+}
